a0715fb13ece3d332ef89a7c6aaaf539ccc2c6d2
Kilian Brachtendorf <Kilian.Brachtendorf@t-online.de>
2019-01-12 22:34:29 +0100

    add test case to check next() is not implemented in fast instances

--- src/test/java/com/github/kilianB/pcg/fast/PcgRSFastTest.java
+++ src/test/java/com/github/kilianB/pcg/fast/PcgRSFastTest.java
@@ -1,20 +1,27 @@
 package com.github.kilianB.pcg.fast;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import com.github.kilianB.pcg.Pcg;
 import com.github.kilianB.pcg.PcgBaseTest;
 
 /**
  * JUnit tests are only used to test methods like distance advance/skip/rewind
  * states and other ordinary functionality. It is not designed to test
- * distributions or statistical properties! <p>
+ * distributions or statistical properties!
+ * <p>
  * 
- * Statistical properties are tested with PractRand evaluation <p> Performance
- * are checked by jmh
+ * Statistical properties are tested with PractRand evaluation
+ * <p>
+ * Performance are checked by jmh
  * 
  * @author Kilian
  *
  */
-class PcgRSFastTest extends PcgBaseTest{
+class PcgRSFastTest extends PcgBaseTest {
 
 	@Override
 	public Pcg getInstance() {
@@ -29,6 +36,18 @@ class PcgRSFastTest extends PcgBaseTest{
 	@Override
 	public boolean isFast() {
 		return true;
+	}
+
+	@Nested
+	class FastSpecific {
+
+		@Test
+		void noNext() {
+			assertThrows(UnsupportedOperationException.class, () -> {
+				getInstance().next(4);
+			});
+		}
+
 	}
 
 }
